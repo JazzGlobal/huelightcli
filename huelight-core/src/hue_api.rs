ca7019fdc5eb051438ab106a6012a3256f342409
chris <waymer147@gmail.com>
2025-11-26 19:38:45 -0500

    oopsies

--- huelight-core/src/hue_api.rs
+++ huelight-core/src/hue_api.rs
@@ -304,7 +304,8 @@ mod tests {
     async fn async_set_light_state_valid_response_returns_model() {
         // Arrange
         let mock_hue_client = MockHueClient::new().with_put_json(|_url, _body| {
-            let serialized_response = r#"[ { "error": { "type": 7, "address": "/lights/2/state/bri", "description": "invalid value, null,, for parameter, bri" } }, { "success": { "/lights/2/state/on": false } }]"#;            Ok(serialized_response.to_string())
+            let serialized_response = r#"[ { "error": { "type": 7, "address": "/lights/2/state/bri", "description": "invalid value, null,, for parameter, bri" } }, { "success": { "/lights/2/state/on": false } }]"#;            
+            Ok(serialized_response.to_string())
         });
 
         let state = LightState::default();
