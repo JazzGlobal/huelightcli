cfa1645a5623e95f95737c2b55d042433c29739e
Chris Gambrell <waymer147@gmail.com>
2025-11-30 08:53:25 -0500

    allow client.rs callers to use customer request headers

--- huelight-core/src/client.rs
+++ huelight-core/src/client.rs
@@ -1,11 +1,32 @@
+use std::collections::HashMap;
+
 use crate::error::{CoreError, CoreResult};
 use async_trait::async_trait;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
+
+pub struct Header {
+    pub name: String,
+    pub value: String,
+}
+
+impl Header {
+    pub fn new<N, V>(name: N, value: V) -> Self
+    where
+        N: Into<String>,
+        V: Into<String>,
+    {
+        Self {
+            name: name.into(),
+            value: value.into(),
+        }
+    }
+}
 
 #[async_trait]
 pub trait HueClient {
-    async fn post_json(&self, url: &str, body: &str) -> CoreResult<String>;
-    async fn get(&self, url: &str) -> CoreResult<String>;
-    async fn put_json(&self, url: &str, body: &str) -> CoreResult<String>;
+    async fn post_json(&self, url: &str, body: &str, headers: Vec<Header>) -> CoreResult<String>;
+    async fn get(&self, url: &str, headers: Vec<Header>) -> CoreResult<String>;
+    async fn put_json(&self, url: &str, body: &str, headers: Vec<Header>) -> CoreResult<String>;
 }
 
 pub struct ReqwestHueClient {
@@ -17,16 +38,28 @@ impl ReqwestHueClient {
     pub fn new(client: reqwest::Client) -> Self {
         Self { client }
     }
+
+    pub fn header_to_header_map(headers: &Vec<Header>) -> HeaderMap {
+        let mut map = HeaderMap::new();
+        for h in headers {
+            let name = HeaderName::from_bytes(h.name.as_bytes()).expect("invalid header name");
+            let value = HeaderValue::from_str(&h.value).expect("invalid header value");
+            map.append(name, value);
+        }
+
+        map
+    }
 }
 
 #[async_trait]
 impl HueClient for ReqwestHueClient {
-    async fn post_json(&self, url: &str, body: &str) -> CoreResult<String> {
+    async fn post_json(&self, url: &str, body: &str, headers: Vec<Header>) -> CoreResult<String> {
         // Implementation for sending a POST request with JSON body
+
         let res = self
             .client
             .post(url)
-            .header("Content-Type", "application/json")
+            .headers(ReqwestHueClient::header_to_header_map(&headers))
             .body(body.to_string())
             .send()
             .await
@@ -35,10 +68,11 @@ impl HueClient for ReqwestHueClient {
         res.text().await.map_err(CoreError::Network)
     }
 
-    async fn get(&self, url: &str) -> CoreResult<String> {
+    async fn get(&self, url: &str, headers: Vec<Header>) -> CoreResult<String> {
         let res = self
             .client
             .get(url)
+            .headers(ReqwestHueClient::header_to_header_map(&headers))
             .send()
             .await
             .map_err(CoreError::Network)?;
@@ -46,11 +80,11 @@ impl HueClient for ReqwestHueClient {
         res.text().await.map_err(CoreError::Network)
     }
 
-    async fn put_json(&self, url: &str, body: &str) -> CoreResult<String> {
+    async fn put_json(&self, url: &str, body: &str, headers: Vec<Header>) -> CoreResult<String> {
         let res = self
             .client
             .put(url)
-            .header("Content-Type", "application/json")
+            .headers(ReqwestHueClient::header_to_header_map(&headers))
             .body(body.to_string())
             .send()
             .await
