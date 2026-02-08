fc700cee300253469c1ed21ceafa7067bb522d07
Chris Gambrell <waymer147@gmail.com>
2026-02-08 18:46:52 -0500

    inputs and buttons no longer stretch the entire container length

--- huelight-angular/frontend/app/components/Setup/Setup.tsx
+++ huelight-angular/frontend/app/components/Setup/Setup.tsx
@@ -7,22 +7,33 @@ import { SimpleInput, SimpleInputButton } from "../SimpleInput/simpleinput"
 
 export function Setup() {
     return (
-        <div className="border-solid border-2 m-2 p-2 rounded-md grid gap-2">
-            <h1>Setup</h1>
-            <SimpleInput label="Bridge IP Address" value="" onChange={() => {}} placeholder="IP Address" />
-            <SimpleInput label="API Key (Hue username)" value="" onChange={() => {}} placeholder="API Key" />
-            
-            <br />
-            <h1>-- OR --</h1>
-            <br />
-            
-            <SimpleInputButton label="Pairing: Press link button then click 'Generate Key' to create key" value="" onChange={() => {}} placeholder="Generate Key" />
-            <SimpleInputButton label="Status: Connected" value="" onChange={() => {}} placeholder="Test Connection" />
-            
-            <div className="m-1">
-                <Button>Save and Continue</Button>
-            </div>
+        <div className="border-solid border-2 m-2 p-2 rounded-md">
+            <div className="grid gap-4 max-w-xl">
+                <h1 className="text-xl font-semibold">Setup</h1>
 
+                <SimpleInput label="Bridge IP Address" value="" onChange={() => { }} placeholder="IP Address" />
+                <SimpleInput label="API Key (Hue username)" value="" onChange={() => { }} placeholder="API Key" />
+
+                <h2 className="pt-4 font-semibold">Pairing</h2>
+
+                <SimpleInputButton
+                    label="Press link button then click 'Generate Key' to create key"
+                    value=""
+                    onChange={() => { }}
+                    placeholder="Generate Key"
+                />
+
+                <SimpleInputButton
+                    label="Status: Connected"
+                    value=""
+                    onChange={() => { }}
+                    placeholder="Test Connection"
+                />
+
+                <div className="pt-2">
+                    <Button>Save and Continue</Button>
+                </div>
+            </div>
         </div>
     )
 }
